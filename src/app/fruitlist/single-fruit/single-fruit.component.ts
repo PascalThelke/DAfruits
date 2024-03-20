2f257a81bdb3120f270b2348631d4a9a0c255e8b
Pascal Thelke <p.thelke@googlemail.com>
2024-03-20 21:25:05 +0100

    child to parent communication via @output

--- src/app/fruitlist/single-fruit/single-fruit.component.ts
+++ src/app/fruitlist/single-fruit/single-fruit.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-single-fruit',
@@ -20,4 +20,10 @@ export class SingleFruitComponent {
       { name: 'Arne P.', text: 'nicht so meins' },
     ],
   };
+
+  @Output()fruitname = new EventEmitter<string>();
+
+  emitName(){
+    this.fruitname.emit(this.fruit.name)
+  }
 }
